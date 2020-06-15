3192600fc2481a4ef093f534d60b25c89f087fc3
Mahak <gmahak1@gmail.com>
2020-06-15 17:36:36 +0530

    refractor code: output of instances command

--- cmd/instances.go
+++ cmd/instances.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 
 	openapi "github.com/sdslabs/gctl/client"
 	"github.com/spf13/cobra"
@@ -19,6 +18,14 @@ var getInstancesCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		token := args[0]
 		auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
-		fmt.Println(client.InstancesApi.FetchIntancesByUser(auth))
+		res, _, err := client.InstancesApi.FetchIntancesByUser(auth)
+		if res.Success && len(res.Data) != 0 {
+			for i := 0; i < len(res.Data); i++ {
+				cmd.Println("Id: "+res.Data[i].Id+"\t", "Name: "+res.Data[i].Name+"\t",
+					"Instance Type: "+res.Data[i].InstanceType+"\t", "Language: "+res.Data[i].Language)
+			}
+		} else {
+			cmd.Println("Error: " + err.Error())
+		}
 	},
 }
