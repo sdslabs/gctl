c4eb48fbcc5f09e91b4754ae61bb5bdb5c7764af
Mahak <gmahak1@gmail.com>
2020-07-03 01:30:33 +0530

    test dbmaker: add tests for all dbmaker commands

--- cmd/dbmaker.go
+++ cmd/dbmaker.go
@@ -10,86 +10,109 @@ import (
 )
 
 var dbName string
+var dbtype string
+var db openapi.Database
 
 func init() {
-	createCmd.AddCommand(dbmakerCmd)
-	fetchCmd.AddCommand(fetchDbCmd)
-	deleteCmd.AddCommand(deleteDbCmd)
+	createCmd.AddCommand(CreateDbCmd(client))
+	fetchCmd.AddCommand(FetchDbCmd(client))
+	deleteCmd.AddCommand(DeleteDbCmd(client))
+}
+
+//CreateDbCmd returns command to create a database
+func CreateDbCmd(client *openapi.APIClient) *cobra.Command {
+	var dbmakerCmd = &cobra.Command{
+		Use:   "db [BEARER_TOKEN]",
+		Short: "Create a database",
+		Args:  cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			token := args[0]
+			dbtype, _ = cmd.Flags().GetString("dbtype")
+			db.Name, _ = cmd.Flags().GetString("name")
+			db.Password, _ = cmd.Flags().GetString("password")
+			if token == "" && dbtype == "" && db.Name == "" && db.Password == "" {
+				token, dbtype, db = middlewares.DbForm()
+			}
+			localVarOptional := &openapi.CreateDBOpts{
+				Database: optional.NewInterface(db),
+			}
+			auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
+			res, _, err := client.DbsApi.CreateDB(auth, dbtype, localVarOptional)
+			if res.Success {
+				cmd.Print("Database created")
+			} else {
+				cmd.Print(err)
+			}
+		},
+	}
+	dbmakerCmd.Flags().StringVarP(&db.Name, "name", "n", "", "Database name")
+	dbmakerCmd.Flags().StringVarP(&db.Password, "password", "p", "", "Database password")
+	dbmakerCmd.Flags().StringVarP(&dbtype, "dbtype", "t", "", "Database type")
+	return dbmakerCmd
+}
+
+//FetchDbCmd returns command to fetch databases of a user
+func FetchDbCmd(client *openapi.APIClient) *cobra.Command {
+	var fetchDbCmd = &cobra.Command{
+		Use:   "db [BEARER_TOKEN]",
+		Short: "Fetch database owned by a user",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			dbName, _ := cmd.Flags().GetString("name")
+			token := args[0]
+			auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
+			if dbName != "" {
+				res, _, err := client.DbsApi.FetchDbByUser(auth, dbName)
+				if res.Success {
+					if len(res.Data) != 0 {
+						for i := 0; i < len(res.Data); i++ {
+							cmd.Println("User: "+res.Data[i].User, "Owner: "+res.Data[i].Owner, "DbUrl: "+res.Data[i].DbUrl, "Port: ", res.Data[i].Port,
+								"Host: "+res.Data[i].HostIp, "Language: "+res.Data[i].Language, "Instance Type: "+res.Data[i].InstanceType)
+						}
+					} else {
+						cmd.Println("No such database")
+					}
+				} else {
+					cmd.Println("Error in fetching the database", err)
+				}
+			} else {
+				res, _, err := client.DbsApi.FetchDbsByUser(auth)
+				if res.Success {
+					if len(res.Data) != 0 {
+						for i := 0; i < len(res.Data); i++ {
+							cmd.Println("User: "+res.Data[i].User, "Owner: "+res.Data[i].Owner, "DbUrl: "+res.Data[i].DbUrl, "Port: ", res.Data[i].Port,
+								"Host: "+res.Data[i].HostIp, "Language: "+res.Data[i].Language, "Instance Type: "+res.Data[i].InstanceType)
+						}
+					} else {
+						cmd.Println("No database for the user")
+					}
+				} else {
+					cmd.Println("Error in fetching the databases", err)
+				}
+			}
+		},
+	}
 	fetchDbCmd.Flags().StringVarP(&dbName, "name", "n", "", "Fetch specific database")
+	return fetchDbCmd
 }
 
-var dbmakerCmd = &cobra.Command{
-	Use:   "db",
-	Short: "Create a database",
-	Args:  cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
-		token, dbtype, database := middlewares.DbForm()
-		localVarOptional := &openapi.CreateDBOpts{
-			Database: optional.NewInterface(database),
-		}
-		auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
-		res, _, err := client.DbsApi.CreateDB(auth, dbtype, localVarOptional)
-		if res.Success {
-			cmd.Print("Database created")
-		} else {
-			cmd.Print(err)
-		}
-	},
-}
-
-var fetchDbCmd = &cobra.Command{
-	Use:   "db [BEARER_TOKEN]",
-	Short: "Fetch database owned by a user",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		dbName, _ := cmd.Flags().GetString("name")
-		token := args[0]
-		auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
-		if dbName != "" {
-			res, _, err := client.DbsApi.FetchDbByUser(auth, dbName)
+//DeleteDbCmd returns command to delete database owned by a user
+func DeleteDbCmd(client *openapi.APIClient) *cobra.Command {
+	var deleteDbCmd = &cobra.Command{
+		Use:   "db [DB_NAME] [BEARER_TOKEN]",
+		Short: "Delete a single database owned by a user",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			dbName := args[0]
+			token := args[1]
+			auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
+			res, _, err := client.DbsApi.DeleteDbByUser(auth, dbName)
 			if res.Success {
-				if len(res.Data) != 0 {
-					for i := 0; i < len(res.Data); i++ {
-						cmd.Println("User: "+res.Data[i].User, "Owner: "+res.Data[i].Owner, "DbUrl: "+res.Data[i].DbUrl, "Port: ", res.Data[i].Port,
-							"Host: "+res.Data[i].HostIp, "Language: "+res.Data[i].Language, "Instance Type: "+res.Data[i].InstanceType)
-					}
-				} else {
-					cmd.Println("No such database")
-				}
+				cmd.Println("Database deleted successfully")
 			} else {
 				cmd.Println(err)
 			}
-		} else {
-			res, _, err := client.DbsApi.FetchDbsByUser(auth)
-			if res.Success {
-				if len(res.Data) != 0 {
-					for i := 0; i < len(res.Data); i++ {
-						cmd.Println("User: "+res.Data[i].User, "Owner: "+res.Data[i].Owner, "DbUrl: "+res.Data[i].DbUrl, "Port: ", res.Data[i].Port,
-							"Host: "+res.Data[i].HostIp, "Language: "+res.Data[i].Language, "Instance Type: "+res.Data[i].InstanceType)
-					}
-				} else {
-					cmd.Println("No database for the user")
-				}
-			} else {
-				cmd.Println(err)
-			}
-		}
-	},
-}
-
-var deleteDbCmd = &cobra.Command{
-	Use:   "db [DB_NAME] [BEARER_TOKEN]",
-	Short: "Delete a single database owned by a user",
-	Args:  cobra.ExactArgs(2),
-	Run: func(cmd *cobra.Command, args []string) {
-		dbName := args[0]
-		token := args[1]
-		auth := context.WithValue(context.Background(), openapi.ContextAccessToken, token)
-		res, _, err := client.DbsApi.DeleteDbByUser(auth, dbName)
-		if res.Success {
-			cmd.Println("Database deleted successfully")
-		} else {
-			cmd.Println(err)
-		}
-	},
+		},
+	}
+	return deleteDbCmd
 }
