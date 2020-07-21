d2937c32e63ad4b91cb96667bcc8f9377134fa2c
Mahak <gmahak1@gmail.com>
2020-07-21 20:15:22 +0530

    cleanup(authentication):remove login and register commands

--- cmd/authentication.go
+++ cmd/authentication.go
@@ -6,74 +6,17 @@ import (
 	_nethttp "net/http"
 
 	openapi "github.com/sdslabs/gctl/client"
-	"github.com/sdslabs/gctl/cmd/middlewares"
 	"github.com/spf13/cobra"
 )
 
 type AuthAPIService interface {
-	Login(ctx _context.Context, login openapi.Login) (openapi.LoginResponse, *_nethttp.Response, error)
 	Refresh(ctx _context.Context, authorization string) (openapi.LoginResponse, *_nethttp.Response, error)
-	Register(ctx _context.Context, user openapi.User) (openapi.InlineResponse200, *_nethttp.Response, error)
 }
 
 var authAPISservice AuthAPIService = client.AuthApi
-var loginCreds openapi.Login
-var registerCreds openapi.User
 
 func init() {
-	rootCmd.AddCommand(LoginCmd(authAPISservice))
-	rootCmd.AddCommand(RegisterCmd(authAPISservice))
 	rootCmd.AddCommand(RefreshCmd(authAPISservice))
-}
-
-//LoginCmd returns command for login
-func LoginCmd(authAPIService AuthAPIService) *cobra.Command {
-	var loginCmd = &cobra.Command{
-		Use:   "login",
-		Short: "Login to get a bearer token ",
-		Run: func(cmd *cobra.Command, args []string) {
-			loginCreds.Email, _ = cmd.Flags().GetString("email")
-			loginCreds.Password, _ = cmd.Flags().GetString("password")
-			if loginCreds.Email == "" && loginCreds.Password == "" {
-				loginCreds = middlewares.LoginForm()
-			}
-			res, _, err := authAPIService.Login(context.Background(), loginCreds)
-			if res.Token != "" {
-				cmd.Println("Token: ", res.Token, "\n", "Expires at: ", res.Expire)
-			} else {
-				cmd.Println("Error:", err)
-			}
-		},
-	}
-	loginCmd.Flags().StringVarP(&loginCreds.Email, "email", "e", "", "Email of the user")
-	loginCmd.Flags().StringVarP(&loginCreds.Password, "password", "p", "", "Password")
-	return loginCmd
-}
-
-//RegisterCmd returns command to register a user to gasper
-func RegisterCmd(authAPIService AuthAPIService) *cobra.Command {
-	var registerCmd = &cobra.Command{
-		Use:   "register",
-		Short: "Register a user",
-		Run: func(cmd *cobra.Command, args []string) {
-			registerCreds.Email, _ = cmd.Flags().GetString("email")
-			registerCreds.Password, _ = cmd.Flags().GetString("password")
-			registerCreds.Username, _ = cmd.Flags().GetString("username")
-			if registerCreds.Email == "" && registerCreds.Password == "" && registerCreds.Username == "" {
-				registerCreds = middlewares.RegisterForm()
-			}
-			res, _, err := authAPIService.Register(context.Background(), registerCreds)
-			if res.Success {
-				cmd.Println(res.Message)
-			} else {
-				cmd.Println(err)
-			}
-		},
-	}
-	registerCmd.Flags().StringVarP(&registerCreds.Email, "email", "e", "", "Email of the user")
-	registerCmd.Flags().StringVarP(&registerCreds.Username, "username", "u", "", "Username")
-	registerCmd.Flags().StringVarP(&registerCreds.Password, "password", "p", "", "Password")
-	return registerCmd
 }
 
 //RefreshCmd returns command to refresh existing token
