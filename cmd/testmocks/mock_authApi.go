fbd5a0d709ce945783d91edc62b28be3da94bdff
mhk19 <mahak_g@cs.iitr.ac.in>
2020-07-22 23:18:30 +0530

    cleanup(cmd/authentication):remove refresh command

--- cmd/testmocks/mock_authApi.go
+++ cmd/testmocks/mock_authApi.go
@@ -50,19 +50,3 @@ func (mr *MockAuthAPIServiceMockRecorder) Login(ctx, email interface{}) *gomock.
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockAuthAPIService)(nil).Login), ctx, email)
 }
-
-// Refresh mocks base method
-func (m *MockAuthAPIService) Refresh(ctx context.Context, authorization string) (openapi.LoginResponse, *http.Response, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Refresh", ctx, authorization)
-	ret0, _ := ret[0].(openapi.LoginResponse)
-	ret1, _ := ret[1].(*http.Response)
-	ret2, _ := ret[2].(error)
-	return ret0, ret1, ret2
-}
-
-// Refresh indicates an expected call of Refresh
-func (mr *MockAuthAPIServiceMockRecorder) Refresh(ctx, authorization interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Refresh", reflect.TypeOf((*MockAuthAPIService)(nil).Refresh), ctx, authorization)
-}
