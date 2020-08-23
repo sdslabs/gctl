2e1aac2577d0c0fb3187fefbdf5405fa09812933
mhk19 <mahak_g@cs.iitr.ac.in>
2020-08-24 00:01:30 +0530

    refactor(client/fetchError):use the stream used everywhere for throwing errors instead of fmt

--- client/fetchError.go
+++ client/fetchError.go
@@ -2,13 +2,12 @@ package openapi
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 func fetchError(b []byte) string {
 	var res InlineResponse400
 	if err := json.Unmarshal(b, &res); err != nil {
-		fmt.Print(err)
+		return "Error in fetching errors"
 	}
 	return res.Error
 }
